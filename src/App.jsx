6dec2abd1c8078aea3cff638c97711f690428467
CeriniA <adriancerini@gmail.com>
2025-06-23 21:34:58 -0300

    add standard lint

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// import { useState } from 'react'
+import TaskManager from './Components/TaskManager/TaskManager'
+import './App.css'
+
+function App() {
+
+  return <TaskManager/>
+}
+
+export default App
